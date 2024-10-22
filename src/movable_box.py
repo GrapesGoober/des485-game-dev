d31a47fa48756f0233c583c0f1ba4e5cb781dcfa
grapsgoober <twinklierre@gmail.com>
2024-10-22 11:01:26 +0700

    #1 modified GridSprites to composition instead of inheritance

--- src/movable_box.py
+++ src/movable_box.py
@@ -1,16 +1,17 @@
 import pygame
-from lib import Frame, GameObject, World
-from src.grid_sprite import GridSprite
+from lib import Frame, GameObject, World, Sprite
+from grid_sprites import GridSprites
 
 SIZE = 100, 100
 COLOR = (255, 255, 255)
 
 class MovableBox(GameObject):
     def __init__(self, grid_position: tuple[int, int]) -> None:
-        self.sprite = GridSprite()
+        self.sprite = Sprite()
         self.sprite.src_image = pygame.Surface(SIZE)
         self.sprite.src_image.fill(COLOR)
-        self.sprite.grid_position = grid_position
+
+        self.grid_sprites = GridSprites(grid_position, self.sprite)
 
     def on_create(self, world: World) -> None:
         world.sprites.add(self.sprite)
@@ -19,7 +20,7 @@ class MovableBox(GameObject):
         for event in frame.events:
             if event.type == pygame.KEYDOWN:
                 match event.key:
-                    case pygame.K_w: self.sprite.grid_y -= 1
-                    case pygame.K_a: self.sprite.grid_x -= 1
-                    case pygame.K_s: self.sprite.grid_y += 1
-                    case pygame.K_d: self.sprite.grid_x += 1
+                    case pygame.K_w: self.grid_sprites.grid_y -= 1
+                    case pygame.K_a: self.grid_sprites.grid_x -= 1
+                    case pygame.K_s: self.grid_sprites.grid_y += 1
+                    case pygame.K_d: self.grid_sprites.grid_x += 1
