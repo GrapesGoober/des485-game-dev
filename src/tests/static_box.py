be13464f7c5105318644c1711e461b995aa3033d
Graps <twinklierre@gmail.com>
2024-10-28 17:21:56 +0700

    #4 added static box

---
+++ src/tests/static_box.py
@@ -0,0 +1,19 @@
+import pygame
+from lib import Frame, GameObject, World, Sprite
+from src.grid_object import GridObject
+
+SIZE = 100, 100
+COLOR = (255, 255, 255)
+
+class StaticBox(GameObject):
+    def __init__(self, grid_position: tuple[int, int]) -> None:
+        self.sprite = Sprite()
+        self.sprite.src_image = pygame.Surface(SIZE)
+        self.sprite.src_image.fill(COLOR)
+        self.sprite.x = grid_position[0] * SIZE[0]
+        self.sprite.y = grid_position[1] * SIZE[1]
+        self.position = GridObject(grid_position)
+
+    def on_create(self, world: World) -> None:
+        world.sprites.add(self.sprite)
+        world.add(self.position)
