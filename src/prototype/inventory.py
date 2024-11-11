c293a098ccb9da488b9fec71077809b4a1ee38c2
Graps <twinklierre@gmail.com>
2024-11-11 19:43:16 +0700

    #14 modified class Inventory to InventoryGUI with subworld

--- src/prototype/inventory.py
+++ src/prototype/inventory.py
@@ -1,27 +1,25 @@
 import pygame
 from lib import Frame, GameObject, World, Sprite
 
-class Inventory(GameObject):
+ITEM_GUI_HEIGHT = 100
+
+class InventoryGUI(GameObject):
     def __init__(self, position: tuple[int, int]) -> None:
-        self.sprite = Sprite()
-        self.items: list[tuple[Sprite, function]] = []
+        self.position = position
+        self.items = World()
+        self.screen: pygame.Surface = None
 
     def on_create(self, world: World) -> None:
-        world.sprites.add(self.sprite)
-
-    def on_remove(self, world):
-        world.sprites.remove(self.sprite)
+        self.screen = pygame.display.get_surface()
 
     def on_update(self, world: World, frame: Frame) -> None:
-        # check mouse click onto item
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        ...
+        self.items.update(frame.events, frame.dt)
+        self.items.draw(self.screen)
 
-        # right now, as a place holder, just use space bar
-        for s, on_item_use in self.items:
-            if pygame.key.get_pressed()[pygame.K_SPACE]:
-                on_item_use()
-
-    def add_item(self, sprite: Sprite, on_item_use: 'function') -> None:
-        self.items.append((sprite, on_item_use))
-        # also, re-adjust item position
+    def add_item_gui(self, item: GameObject) -> None:
+        self.items.add(item)
+    
+    def get_item_gui_position(self, item: GameObject) -> tuple[int, int]:
+        x = self.position[0]
+        y = self.position[1] + self.items.gameobjects.index(item) * ITEM_GUI_HEIGHT
+        return x, y
--- src/prototype/items/test_item.py
+++ src/prototype/items/test_item.py
@@ -1,6 +1,7 @@
+from typing import Any
 import pygame
 from lib import Frame, GameObject, World, Sprite
-from src.prototype.inventory import Inventory
+from src.prototype.inventory import InventoryGUI
 from src.prototype.rat import Rat
 from src.grid_position import GridPosition
 
@@ -9,16 +10,18 @@ COLOR = (255, 255, 255)
 PLAYER_KEY = "PLAYER"
 INVENTORY_KEY = "INVENTORY"
 
+IMAGE = pygame.Surface(SIZE)
+IMAGE.fill(COLOR)
+
 class TestItemProp(GameObject):
     def __init__(self, grid_position: tuple[int, int]) -> None:
         self.sprite = Sprite()
-        self.sprite.src_image = pygame.Surface(SIZE)
-        self.sprite.src_image.fill(COLOR)
+        self.sprite.src_image = IMAGE
         self.sprite.x = grid_position[0] * SIZE[0]
         self.sprite.y = grid_position[1] * SIZE[1]
         self.position = GridPosition(grid_position)
         self.player: Rat = None
-        self.inventory: Inventory = None
+        self.inventory: InventoryGUI = None
 
     def on_create(self, world: World) -> None:
         world.sprites.add(self.sprite)
@@ -34,13 +37,26 @@ class TestItemProp(GameObject):
         # check player collision to add itself to inventory
         for n in self.position.get_neighbours(world):
             if n.parent_object == self.player:
-
-                def on_item_use():
-                    print("hi, using item")
-
-                self.inventory.add_item(
-                    sprite      = self.sprite,
-                    on_item_use = on_item_use
-                )
-
+                item_gui = TestItemGUI(self.inventory)
+                self.inventory.add_item_gui(item_gui)
                 world.remove(self)
+
+class TestItemGUI(GameObject):
+    def __init__(self, inventory: InventoryGUI) -> None:
+        self.sprite = Sprite()
+        self.sprite.src_image = IMAGE
+        self.inventory = inventory
+
+    def on_create(self, world: World) -> None:
+        world.sprites.add(self.sprite)
+
+    def on_remove(self, world: World) -> None:
+        world.sprites.remove(self.sprite)
+
+    def on_update(self, world: World, frame: Frame) -> None:
+        self.sprite.position = self.inventory.get_item_gui_position(self)
+        # right now, as a place holder, just use space bar
+        # realistically, use mouse position
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            print("hi, using item")
+            world.remove(self)
