6018d1018eb5cf1ddda3ad56e4878baa7f05bb39
Graps <twinklierre@gmail.com>
2024-11-11 20:07:54 +0700

    #14 refactor inventory gui to simple collection

--- src/prototype/inventory.py
+++ src/prototype/inventory.py
@@ -6,20 +6,12 @@ ITEM_GUI_HEIGHT = 100
 class InventoryGUI(GameObject):
     def __init__(self, position: tuple[int, int]) -> None:
         self.position = position
-        self.items = World()
-        self.screen: pygame.Surface = None
-
-    def on_create(self, world: World) -> None:
-        self.screen = pygame.display.get_surface()
-
-    def on_update(self, world: World, frame: Frame) -> None:
-        self.items.update(frame.events, frame.dt)
-        self.items.draw(self.screen)
+        self.items: list[GameObject] = []
 
     def add_item_gui(self, item: GameObject) -> None:
-        self.items.add(item)
+        self.items.append(item)
     
     def get_item_gui_position(self, item: GameObject) -> tuple[int, int]:
         x = self.position[0]
-        y = self.position[1] + self.items.gameobjects.index(item) * ITEM_GUI_HEIGHT
+        y = self.position[1] + self.items.index(item) * ITEM_GUI_HEIGHT
         return x, y
--- src/prototype/items/test_item.py
+++ src/prototype/items/test_item.py
@@ -13,7 +13,7 @@ INVENTORY_KEY = "INVENTORY"
 IMAGE = pygame.Surface(SIZE)
 IMAGE.fill(COLOR)
 
-class TestItemProp(GameObject):
+class TestItem(GameObject):
     def __init__(self, grid_position: tuple[int, int]) -> None:
         self.sprite = Sprite()
         self.sprite.src_image = IMAGE
@@ -22,6 +22,7 @@ class TestItemProp(GameObject):
         self.position = GridPosition(grid_position)
         self.player: Rat = None
         self.inventory: InventoryGUI = None
+        self.is_picked = False
 
     def on_create(self, world: World) -> None:
         world.sprites.add(self.sprite)
@@ -34,29 +35,20 @@ class TestItemProp(GameObject):
         world.sprites.remove(self.sprite)
 
     def on_update(self, world: World, frame: Frame) -> None:
-        # check player collision to add itself to inventory
-        for n in self.position.get_neighbours(world):
-            if n.parent_object == self.player:
-                item_gui = TestItemGUI(self.inventory)
-                self.inventory.add_item_gui(item_gui)
+
+        if self.is_picked == False:
+            # check player collision to add itself to inventory GUI
+            for n in self.position.get_neighbours(world):
+                if n.parent_object == self.player:
+                    self.inventory.add_item_gui(self)
+                    self.is_picked = True
+
+        if self.is_picked == True:
+            # the inventory GUI will figure what coordinate to pose as
+            self.sprite.position = self.inventory.get_item_gui_position(self)
+            # right now, as a place holder, just use space bar
+            # realistically, use mouse position
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                print("hi, using item")
+                print(f"i have the player here {self.player} what should I do with it?")
                 world.remove(self)
-
-class TestItemGUI(GameObject):
-    def __init__(self, inventory: InventoryGUI) -> None:
-        self.sprite = Sprite()
-        self.sprite.src_image = IMAGE
-        self.inventory = inventory
-
-    def on_create(self, world: World) -> None:
-        world.sprites.add(self.sprite)
-
-    def on_remove(self, world: World) -> None:
-        world.sprites.remove(self.sprite)
-
-    def on_update(self, world: World, frame: Frame) -> None:
-        self.sprite.position = self.inventory.get_item_gui_position(self)
-        # right now, as a place holder, just use space bar
-        # realistically, use mouse position
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            print("hi, using item")
-            world.remove(self)
