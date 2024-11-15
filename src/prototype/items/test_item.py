14b8d18c813eddda91514e19e4b743de6f04077f
grapsgoober <twinklierre@gmail.com>
2024-11-15 18:05:38 +0700

    #31 divided test item into three classes: base, prop, and inventory gui

--- src/prototype/items/test_item.py
+++ src/prototype/items/test_item.py
@@ -11,16 +11,10 @@ COLOR = (255, 255, 255)
 IMAGE = pygame.Surface(SIZE)
 IMAGE.fill(COLOR)
 
-class TestItem(GameObject):
-    def __init__(self, player: Rat, inventory: InventoryGUI, grid_position: tuple[int, int]) -> None:
+class BaseTestItem(GameObject):
+    def __init__(self) -> None:
         self.sprite = Sprite()
         self.sprite.src_image = IMAGE
-        self.sprite.x = grid_position[0] * SIZE[0]
-        self.sprite.y = grid_position[1] * SIZE[1]
-        self.position = GridPosition(grid_position)
-        self.player: Rat = player
-        self.inventory: InventoryGUI = inventory
-        self.is_picked = False
 
     def on_create(self, world: World) -> None:
         world.sprites.add(self.sprite)
@@ -28,21 +22,38 @@ class TestItem(GameObject):
     def on_remove(self, world):
         world.sprites.remove(self.sprite)
 
+class TestItemProp(BaseTestItem):
+    def __init__(self, player: Rat, inventory: InventoryGUI, grid_position: tuple[int, int]) -> None:
+        super().__init__()
+        self.sprite.x = grid_position[0] * SIZE[0]
+        self.sprite.y = grid_position[1] * SIZE[1]
+        self.position = GridPosition(grid_position)
+        self.player: Rat = player
+        self.inventory: InventoryGUI = inventory
+        self.is_picked = False
+
     def on_update(self, world: World, frame: Frame) -> None:
-
-        if self.is_picked == False:
-            # check player collision to add itself to inventory GUI
-            for n in self.position.get_neighbours(world):
-                if n.parent_object == self.player:
-                    self.inventory.add_item_gui(self)
-                    self.is_picked = True
-
-        if self.is_picked == True:
-            # the inventory GUI will figure what coordinate to pose as
-            self.sprite.position = self.inventory.get_item_gui_position(self)
-            # right now, as a place holder, just use space bar
-            # realistically, use mouse position
-            if pygame.key.get_pressed()[pygame.K_SPACE]:
-                print("hi, using item")
-                print(f"i have the player here {self.player} what should I do with it?")
+        # check player collision to add itself to inventory GUI
+        for n in self.position.get_neighbours(world):
+            if n.parent_object == self.player:
+                item_gui = TestItemInventoryGUI(self.player, self.inventory)
+                world.add(item_gui)
+                self.inventory.add_item_gui(item_gui)
                 world.remove(self)
+
+class TestItemInventoryGUI(BaseTestItem):
+   
+    def __init__(self, player: Rat, inventory: InventoryGUI) -> None:
+        super().__init__()
+        self.player: Rat = player
+        self.inventory: InventoryGUI = inventory
+
+    def on_update(self, world, frame):
+        # the inventory GUI will figure what coordinate to pose as
+        self.sprite.position = self.inventory.get_item_gui_position(self)
+        # right now, as a place holder, just use space bar
+        # realistically, use mouse position
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            print("hi, using item")
+            print(f"i have the player here {self.player} what should I do with it?")
+            world.remove(self)
--- worlds/levels.py
+++ worlds/levels.py
@@ -2,7 +2,7 @@
 from src.prototype.level_manager import LevelManager
 from src.prototype.items.cheese import Cheese
 from src.prototype.inventory import InventoryGUI
-from src.prototype.items.test_item import TestItem
+from src.prototype.items.test_item import TestItemProp
 from src.prototype.rat import Rat
 from src.prototype.tree import Tree
 
@@ -11,7 +11,7 @@ def create_level_test_1(player: Rat, inventory_gui: InventoryGUI, level_manager:
         Tree(player, (6, 6), False),
         Tree(player, (7, 7), False),
         Tree(player, (8, 8), True),
-        TestItem(player, inventory_gui, (10, 8)),
+        TestItemProp(player, inventory_gui, (10, 8)),
         Cheese(
             player, 
             callback= lambda: level_manager.transition(
@@ -26,7 +26,7 @@ def create_level_test_2(player: Rat, inventory_gui: InventoryGUI):
         Tree(player, (9, 19), False),
         Tree(player, (9, 21), False),
         Tree(player, (11, 21), True),
-        TestItem(player, inventory_gui, (10, 20)),
-        TestItem(player, inventory_gui, (10, 30)),
-        TestItem(player, inventory_gui, (10, 40)),
+        TestItemProp(player, inventory_gui, (10, 20)),
+        TestItemProp(player, inventory_gui, (10, 30)),
+        TestItemProp(player, inventory_gui, (10, 40)),
     ]
